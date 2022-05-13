609e58153c5e1a45de440c6a976a00ed0a1d5438
Srinath Venkatesh <srinathv@udel.edu>
2022-05-12 23:57:08 -0400

    added dark mode toggle

--- components/MainMenu.tsx
+++ components/MainMenu.tsx
@@ -1,14 +1,30 @@
+import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
+import { DarkModeToggleSwitch } from "./DarkModeToggleSwitch";
 
 export default function MainMenu({ menuType }: {
     menuType: "main-page" | "top-menu"
 }): JSX.Element {
+    const [isDarkModeToggle, setIsDarkModeToggle] = useState<boolean>(false);
+    // Dark Mode Toggle
+    useEffect(() => {
+        const mainPage = document.getElementById("mainPage");
+        if (mainPage !== null){
+            if (isDarkModeToggle) {
+                mainPage.className = `${styles.containerLight}`;
+            }
+            else {
+                mainPage.className = `${styles.container}`;
+            }
+        }
+    }, [isDarkModeToggle]);
+
     return(
         <ul>
-            {/* {menuType === "top-menu" && 
+            {menuType === "top-menu" && 
                 <li>
-                    {"Mode: "}<DarkModeToggle checked={!isDarkModeToggle} onChange={()=> setDarkModeToggle(!isDarkModeToggle)} sx={{ m: 1 }}/>
-                </li>} */}
+                    {"Mode: "}<DarkModeToggleSwitch checked={!isDarkModeToggle} onChange={()=> setIsDarkModeToggle(!isDarkModeToggle)} sx={{ m: 1 }}/>
+                </li>}
             {menuType === "top-menu" && 
                 <li>
                     <a className={styles.menuLink} href="https://github.com/srinathv31" target={"_blank"} rel="noreferrer">Home</a>
