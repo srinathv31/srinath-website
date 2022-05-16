9096c7008a0817b31565646fc36c71f0681dcd30
Srinath Venkatesh <srinathv@udel.edu>
2022-05-16 13:23:24 -0400

    resized about picture

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,10 +3,11 @@ import { deepPurple } from "@mui/material/colors";
 import styles from "../styles/Home.module.css";
 
 export default function About(): JSX.Element {
+    const windowWidth = window.innerWidth;
     return(
         <>
             <main className={styles.main}>
-                <Avatar sx={{ bgcolor: deepPurple[500], width: 200, height: 200 }}>SV</Avatar>
+                <Avatar sx={{ bgcolor: deepPurple[500], width: windowWidth*0.15, height: windowWidth*0.15 }}>SV</Avatar>
                 <div className={styles.aboutDescription}>
                     <h1>
                         {"Hi, again 👋 I'm"}<div className={styles.nameHeader}>Srinath Venkatesh</div>
