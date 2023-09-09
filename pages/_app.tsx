ac032e0c59af911708af28cd997e06886d1d4ddf
Srinath Venkatesh <srinathvb1@gmail.com>
2023-09-09 16:15:04 -0400

    analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,21 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { useState } from "react";
 import { globalPropsContext } from "../components/ContextHooks/GlobalPropsContext";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
     const [isDarkModeToggle, setIsDarkModeToggle] = useState<boolean>(false);
     const globalProps = { isDarkModeToggle, setIsDarkModeToggle };
 
     return (
-        <globalPropsContext.Provider value={globalProps}>
-            <Layout>
-                <Component {...pageProps} id="mainPage" className="dark"/>
-            </Layout>
-        </globalPropsContext.Provider>
+        <>
+            <globalPropsContext.Provider value={globalProps}>
+                <Layout>
+                    <Component {...pageProps} id="mainPage" className="dark" />
+                </Layout>
+            </globalPropsContext.Provider>
+            <Analytics />
+        </>
     );
 }
 
